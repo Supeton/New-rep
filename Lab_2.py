3239ad6f427e6f66ea81cc56ca6ec1f82fb812d6
Supeton <sashamelnik131@gmail.com>
2025-03-25 14:20:54 +0200

    commit

---
+++ Lab_2.py
@@ -0,0 +1,13 @@
+while True :
+    msg=input("please input a msg ")
+    if msg=="Добрий день" :
+        print("Добрий день")
+    elif msg=="Слава Україні" :
+        print("Героям слава")
+    elif msg=="Hello" :
+        print("Hi")
+    elif msg=="pa pa" :
+        print("the end")
+        break
+    else : 
+        print ("Спробуй ще")
